9c498bfd2ad845ad87dc0864aa2434d78a20d64d
chand <chandra.kantheti@gmail.com>
2025-04-08 14:24:28 -0700

    FrontEnd app.js and index.js and coursetable base added

---
+++ FrontEnd/grades_web_app/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState, usestate} from 'react';
+import LoginPage from './webComponents/LoginPage';
+import studentDash from './studTeachCourse/studentDash';
+import teachDash from './studTeachCourse/teachDash';
+
+function App() {
+  const [user, setUser] = useState(null);
+  
+  const handleLogin = (userData) => {
+    setUser(userData);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  if (!user) return <LoginPage onLogin={handleLogin} />
+  if (user.role === 'student') return <studentDash user={user} onLogout={handleLogout} />;
+  if (user.role === 'teacher') return <teachDash user={user} onLogout={handleLogout} />;
+
+  return <div>Unknown Role</div>;
+}
+
+export default App;
---
+++ FrontEnd/grades_web_app/src/webComponents/courseTable.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function CourseTable({courses, adding, removing, isStudent}) {
+  return (
+    <table className="course-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Teacher</th>
+          <th>Time</th>
+          <th>Enrolled</th>
+          {isStudent && <th>Action</th>}
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  );
+}
