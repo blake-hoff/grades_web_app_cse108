0790af9419b89720c436bdf7445250baba593212
Parthib Roy <proy@lbl.gov>
2025-04-10 11:05:45 -0700

    fix: add proxy to 'package.json' and use relative API path when fetching login

--- FrontEnd/grades_web_app/src/webComponents/LoginPage.js
+++ FrontEnd/grades_web_app/src/webComponents/LoginPage.js
@@ -6,7 +6,7 @@ function LoginPage({ onLogin }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://127.0.0.1:5000/login', {
+    const response = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
