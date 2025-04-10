fc37fe32549151fb8943eb8c8964fecbee150519
chand <chandra.kantheti@gmail.com>
2025-04-10 11:47:10 -0700

    Mini teacher change

---
+++ FrontEnd/grades_web_app/src/studTeachCourse/teachDash.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react';
+
+function TeacherDashboard({user, onLogOut}) {
+  const [courses, setCourses] = useState([]);
+  const [selectCourses, setSelectCourses] = useState(null);
+  const [students, setStudents] = useState([]);
+
+  
+}
+
+export default TeacherDashboard
